672865ed7d6c3ce42773a8d9a7a3e4ee395b8b10
shuvojit007 <shuvojitkar241@gmail.com>
2016-12-23 20:41:55 +0600

    4th Commit 12/23/2016 8:41 PM

--- app/src/main/java/com/example/shobojit/siuapp/Home/MainActivity.java
+++ app/src/main/java/com/example/shobojit/siuapp/Home/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -51,7 +52,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         LoadHomeFragment();
 
         if(haveNetworkConnection()==false){
-            AlertMessage.showMessage(con,"Alert","No Internet Connection","OK",R.drawable.alertpic);
+            //AlertMessage.showMessage(con,"Alert","No Internet Connection","OK",R.drawable.alertpic);
+            Snackbar.make(findViewById(R.id.toolbar),"No Internet Connection",Snackbar.LENGTH_LONG)
+                    .show();
         }
     }
 
