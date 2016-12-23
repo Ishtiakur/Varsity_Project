c0f2b28de5122b792e0523957d857cdb2b8170ef
shuvojit007 <shuvojitkar241@gmail.com>
2016-12-24 00:21:41 +0600

    5th Commit 12/24/2016 12:21AM

--- app/src/main/java/com/example/shobojit/siuapp/Home/FragmentContainer/Home.java
+++ app/src/main/java/com/example/shobojit/siuapp/Home/FragmentContainer/Home.java
@@ -2,15 +2,15 @@ package com.example.shobojit.siuapp.Home.FragmentContainer;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
@@ -18,15 +18,7 @@ import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.example.shobojit.siuapp.R;
 import com.facebook.drawee.view.SimpleDraweeView;
-
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.HashMap;
-
-/**
- * Created by SHOBOJIT on 12/13/2016.
- */
 
 public class Home extends Fragment  implements  BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
     TextView ttday,ttdate,ttmonth,tttime;
@@ -34,34 +26,67 @@ public class Home extends Fragment  implements  BaseSliderView.OnSliderClickList
     String HomeText , ChairText, VcText;
     WebView wv1,wv2,wv3;
     SimpleDraweeView ChairmanImage,VCImage;
+    FloatingActionButton fab,fab_man,fab_exit;
+    Animation fab_open,fab_clos,fab_rotate,fab_rotate_back;
     View v;
+    boolean isOpen =false;
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         v=inflater.inflate(R.layout.home_fragment,container,false);
-  /*    ttday= (TextView) v.findViewById(R.id.ttday);
-        ttdate= (TextView) v.findViewById(R.id.ttdate);
-        ttmonth= (TextView) v.findViewById(R.id.ttmonth);
-        String []day = {"Sunday","Monday","Tuesday","Wednesday","Wednesday","Friday","Saturday"};
-        Calendar c = Calendar.getInstance();
-        ttday.setText(day[c.getTime().getDay()]);
-        SimpleDateFormat df = new SimpleDateFormat("dd");
-        ttdate.setText(df.format(c.getTime()));
-        SimpleDateFormat d2 = new SimpleDateFormat("MMMMMM");
-        ttmonth.setText( d2.format(c.getTime()));*/
+
         Text ();
-        ImageInitailaization(v);
         TextViewIntailization(v);
         SliderIntailization(v);
+        FloatingIntialization(v);
+        ImageInitailaization(v);
 
-        String text = "<html><body>"
-                + "<p align=\"justify\">"+"<font size=\"3.5\">"+getString(R.string.chairman1)+"</font>"+"</p> "
-                + "<p align=\"justify\">"+"<font size=\"3.5\">"+getString(R.string.chairman2)+"</font>"+ "</p> "
-                + "<p align=\"justify\">"+"<font size=\"3.5\">"+getString(R.string.chairman3)+"</font>"+ "</p> "
-                + "<p align=\"justify\">"+"<font size=\"3.5\">"+getString(R.string.chairman4)+"</font>"+ "</p> "
-                + "</body></html>";
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                fab_man.startAnimation(fab_clos);
+                    fab_exit.startAnimation(fab_clos);
+                        fab.startAnimation(fab_rotate_back);
+                    fab_exit.setVisibility(View.GONE);
+                    fab_man.setVisibility(View.GONE);
+                    fab_man.setClickable(false);
+                    fab_exit.setClickable(false);
+                    isOpen=false;
+                }
+                else {
+                    fab_man.startAnimation(fab_open);
+                    fab_exit.startAnimation(fab_open);
+                    fab.startAnimation(fab_rotate);
+                    fab_man.setClickable(true);
+                    fab_exit.setClickable(true);
 
+                    isOpen=true;
+
+                }
+            }
+        });
         return v;
     }
+
+
+
+
+
+    public void FloatingIntialization(View v){
+        fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab_man = (FloatingActionButton) v.findViewById(R.id.fab_man);
+        fab_exit = (FloatingActionButton) v.findViewById(R.id.fab_exit);
+
+
+        //For Animation
+        fab_open = AnimationUtils.loadAnimation(getContext(),R.anim.fab_open);
+        fab_clos = AnimationUtils.loadAnimation(getContext(),R.anim.fab_close);
+        fab_rotate = AnimationUtils.loadAnimation(getContext(),R.anim.fab_rotat);
+        fab_rotate_back= AnimationUtils.loadAnimation(getContext(),R.anim.fab_rotate_back);
+
+    }
+
+
 
     public void Text (){
         ChairText = "<html><body>"
@@ -86,6 +111,7 @@ public class Home extends Fragment  implements  BaseSliderView.OnSliderClickList
     }
 
 
+
     public void TextViewIntailization(View v){
         wv1 = (WebView) v.findViewById(R.id.NormalText);
         wv1.loadData(HomeText, "text/html", "utf-8");
@@ -96,6 +122,8 @@ public class Home extends Fragment  implements  BaseSliderView.OnSliderClickList
         wv3 = (WebView) v.findViewById(R.id.vctext);
         wv3.loadData(VcText, "text/html", "utf-8");
     }
+
+
 
 
     public void SliderIntailization(View v){
@@ -109,7 +137,6 @@ public class Home extends Fragment  implements  BaseSliderView.OnSliderClickList
         file_maps.put("1st Convocation of SIU ",R.drawable.ca);
         file_maps.put("Physics Olympiad ",R.drawable.physics2);
         file_maps.put("SIU Campus", R.drawable.s5);
-
          for(String name : file_maps.keySet()){
             TextSliderView textSliderView = new TextSliderView(getContext());
             // initialize a SliderLayout
@@ -145,28 +172,26 @@ public class Home extends Fragment  implements  BaseSliderView.OnSliderClickList
         VCImage.setImageURI(Uri.parse(VCImagepath));
 
     }
+
+
     @Override
     public void onStop() {
         // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
         mDemoSlider.stopAutoCycle();
         super.onStop();
     }
-
     @Override
     public void onSliderClick(BaseSliderView slider) {
 
     }
-
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
     }
-
     @Override
     public void onPageSelected(int position) {
 
     }
-
     @Override
     public void onPageScrollStateChanged(int state) {
 
